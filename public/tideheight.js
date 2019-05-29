d0775f7652ca9550665896ebc50fee47804c8b68
elainecarey22 <46967987+elainecarey22@users.noreply.github.com>
2019-05-29 11:38:11 +0100

    File now in public folder

---
+++ public/tideheight.js
@@ -0,0 +1,63 @@
+/*
+** get current date & two days from now
+** to display at the top of the graph
+*/
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var initial = new Date();
+var startDate = initial.toLocaleDateString("en-US", options);
+initial.setDate(initial.getDate() + 2);
+var endDate = initial.toLocaleDateString("en-US", options);
+
+function drawTable(tideHeight) {
+    Highcharts.chart('tide', {
+        chart: {
+            type: 'line',
+            zoomType: 'x',
+        },
+        navigator: {
+            adaptToUpdatedData: false,
+            series: {
+                data: tideHeight
+            }
+        },
+        tooltip: {
+            crosshairs: true
+        },
+        scrollbar: {
+            liveRedraw: false
+        },
+        title: {
+            text: 'Sea Surface Height'
+        },
+        subtitle: {
+            text: startDate + ' - ' + endDate
+        },
+        xAxis: {
+            type: 'datetime',
+        },
+        yAxis: {
+            floor: -2.2
+        },
+        series: [{
+            name: 'Sea Surface Height (m)',
+            data: tideHeight,
+            dataGrouping: {
+                enabled: false
+            }
+        }],
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        }
+    })
+}
